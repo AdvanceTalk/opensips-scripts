b290d4da07f8a224879165f094c78a5ae380373c
agree <37550360+greenbea@users.noreply.github.com>
2020-11-03 14:46:58 -0500

    Create acc-create.sql

---
+++ scripts/acc-create.sql
@@ -0,0 +1,22 @@
+DROP TABLE ACC;
+
+CREATE TABLE acc (
+    id SERIAL PRIMARY KEY NOT NULL,
+    method VARCHAR(16) DEFAULT '' NOT NULL,
+    from_tag VARCHAR(64) DEFAULT '' NOT NULL,
+    to_tag VARCHAR(64) DEFAULT '' NOT NULL,
+    callid VARCHAR(64) DEFAULT '' NOT NULL,
+    sip_code VARCHAR(3) DEFAULT '' NOT NULL,
+    sip_reason VARCHAR(32) DEFAULT '' NOT NULL,
+    accountcode text DEFAULT '' NOT NULL,
+    source text NOT NULL,
+    destination TEXT DEFAULT '' NOT NULL,
+    src_ip TEXT DEFAULT '' NOT NULL,
+    dst_ip TEXT NOT NULL,
+    direction text default '' NOT null,
+    time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    duration INTEGER DEFAULT 0 NOT NULL,
+    ms_duration INTEGER DEFAULT 0 NOT NULL,
+    setuptime INTEGER DEFAULT 0 NOT NULL,
+    created TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL
+);
