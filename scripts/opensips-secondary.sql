6aeb4b6a5a22616e915ac841968e34a22f5d1372
agree <37550360+greenbea@users.noreply.github.com>
2020-11-18 15:37:32 -0500

    Add clusterer table

--- scripts/opensips-secondary.sql
+++ scripts/opensips-secondary.sql
@@ -67,3 +67,20 @@ CREATE TABLE dialog (
     rt_on_timeout VARCHAR(64) DEFAULT NULL,
     rt_on_hangup VARCHAR(64) DEFAULT NULL
 );
+
+INSERT INTO version (table_name, table_version) values ('clusterer','4');
+CREATE TABLE clusterer (
+    id SERIAL PRIMARY KEY NOT NULL,
+    cluster_id INTEGER NOT NULL,
+    node_id INTEGER NOT NULL,
+    url VARCHAR(64) NOT NULL,
+    state INTEGER DEFAULT 1 NOT NULL,
+    no_ping_retries INTEGER DEFAULT 3 NOT NULL,
+    priority INTEGER DEFAULT 50 NOT NULL,
+    sip_addr VARCHAR(64),
+    flags VARCHAR(64),
+    description VARCHAR(64),
+    CONSTRAINT clusterer_clusterer_idx UNIQUE (cluster_id, node_id)
+);
+
+ALTER SEQUENCE clusterer_id_seq MAXVALUE 2147483647 CYCLE;
