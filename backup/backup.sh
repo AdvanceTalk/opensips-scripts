096a3e64816fa19150a19844f9d1f73a01bbd40c
agree <37550360+greenbea@users.noreply.github.com>
2020-11-25 11:35:44 -0500

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -9,7 +9,9 @@ mkdir -p $primary_db_backup_dir
 #mkdir -p $secondary_db_backup_dir
 mkdir -p $cfg_backup_dir
 
-find $backup_dir/*.sql -mtime +3 -exec rm {} \;
+find $cfg_backup_dir/*.cfg -mtime +2 -exec rm {} \;
+find $primary_db_backup_dir/*.sql -mtime +3 -exec rm {} \;
+find $secondary_db_backup_dir/*.sql -mtime +3 -exec rm {} \;
 
 cp /etc/opensips/opensips.cfg $cfg_backup_dir/$(date +%Y-%m-%d)_opensips.cfg
 pg_dump -Fc --host=localhost --port=5432 --user=opensips --schema=public -f /$primary_db_backup_dir/opensips_$(date +%Y-%m-%d).sql --verbose
